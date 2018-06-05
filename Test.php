e27f2e262ec6e7456712668ec49a73c949f1d945
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-06-05 12:05:50 +0900

    CI test

--- Test.php
+++ Test.php
@@ -2,9 +2,10 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
-  	}
+    public function testOnePlusOne() {
+-       $this->assertEquals(1+1,1);
++       $this->assertEquals(1+1,2);
+    }
 }
 
 ?>
